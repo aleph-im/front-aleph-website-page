36cdb67e2e91240b5e11de4d07c0b0e1f1e5d31f
BjrInt <BjrInt@users.noreply.github.com>
2023-05-11 14:04:04 +0000

    Deploying to gh-pages from @ aleph-im/front-aleph-website-page@eab8f115205a95c181de62c2f734b637a788018f 🚀

--- _next/static/0DQKgVANxrMgui_hA0twt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f68b770cd691c835.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],"/legal-mentions":["static/chunks/pages/legal-mentions-494a393fe7acc076.js"],sortedPages:["/","/_app","/_error","/legal-mentions"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
